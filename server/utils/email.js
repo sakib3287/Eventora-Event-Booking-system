88426ff8cc9d2a2e9358da4c3d73b6b23c6dc58d
sakib malik <sakibmalik328725@gmail.com>
2026-06-25 02:31:49 +0530

    smtp resolve

--- server/utils/email.js
+++ server/utils/email.js
@@ -3,20 +3,20 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const transporter = nodemailer.createTransport({
-    // service: 'gmail',
-     host: "smtp.gmail.com",
-     port: 587,
-     secure: false,
-     requireTLS: true,
-     logger: true,
-     debug: true,
-     
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    }
+
+    const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
 });
+
 
 const sendBookingEmail = async (userEmail, userName, eventTitle) => {
     try {
